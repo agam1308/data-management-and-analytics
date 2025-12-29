030dda4ae396135fe40ac52f640a66f022592f6f
Agam Makhija <agamy2k@gmail.com>
2025-12-29 17:34:36 +0530

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/data-management-and-analytics',
 };
 
 export default nextConfig;
